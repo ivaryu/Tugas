c795ab8831b5b5f8b50ae27d6fa85feb70154826
Faisal <144821955+ivaryu@users.noreply.github.com>
2023-09-12 23:12:57 +0800

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,11 @@
+print("Kalkulator Assignment")
+print("1. Equal")
+
+bil_1 = int(input("bilangan pertama = "))
+bil_2 = int(input("bilangan kedua = "))
+pilih = int(input("Pilih Operasi = "))
+
+if pilih == 1
+    print("Hasil")
+    equal = bil_1 == bil_2
+elif
